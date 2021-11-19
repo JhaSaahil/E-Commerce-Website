a8b61a4dca28d165c1f03e80a1ed986059bbd5c4
Saahil <saahilkumarjha@gmail.com>
2021-11-19 10:15:46 +0530

    Initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Carriage</h1>
+      <Router>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Route exact path='/' component={HomeScreen} />
+            <Route path='/product/:id' component={ProductScreen} />
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
